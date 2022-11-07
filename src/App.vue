d26471ccf46e8b925804b2c3e6e11545ebf5f39b
GtDavide01 <davidefiorini01@gmail.com>
2022-11-07 14:00:21 +0100

    prima milestone , 'AppTitle inserita'

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<!-- Descrizione:
+Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!"
+Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron. -->
+
+<!-- SCRIPT -->
+<script>
+// <!-- import -->
+import AppTitle from "./components/AppTitle.vue";
+
+  export default {
+    data() {
+        return {};
+    },
+    components: { AppTitle }
+}
 </script>
 
+<!-- HTML -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section>
+    <AppTitle />
+  </section>
 </template>
 
+<!-- CSS -->
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+        data(){
+            return{
+                titleUser: "La mia prima app con Vite",
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <h1>{{titleUser}}</h1>
+</template>
+
+
+<style scoped >
+ h1{
+    font-size: 3rem;
+    font-style: oblique;
+    text-align: center;
+ }
+</style>
